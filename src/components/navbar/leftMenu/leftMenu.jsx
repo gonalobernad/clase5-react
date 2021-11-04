b8799212139579e8b8e299b29d10840633fbf900
Gonzalo Bernad <gonzalobernad60@gmail.com>
2021-11-04 06:12:45 -0300

    Agrego estilos para sidebar

--- src/components/navbar/leftMenu/leftMenu.jsx
+++ src/components/navbar/leftMenu/leftMenu.jsx
@@ -1,8 +1,27 @@
-import { List, ListItem, ListItemText, SwipeableDrawer } from "@mui/material";
+import { Divider, IconButton, List, ListItem, ListItemText, SwipeableDrawer } from "@mui/material";
+import { styled, useTheme } from '@mui/material/styles';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { useState } from "react";
+import remaxColor from '../../utils/remaxColor';
 
+const iOS =
+typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+const drawerWidth = 240;
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
 function LeftMenu({open, onOpen, onClose}) {
+
+    const theme = useTheme();
 
     const categoriesObj = [
         {
@@ -22,10 +41,30 @@ function LeftMenu({open, onOpen, onClose}) {
 
     return ( 
         <SwipeableDrawer
+            color='primary'
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                  width: drawerWidth,
+                  boxSizing: 'border-box',
+                  background: remaxColor[500],
+                  color: '#fff'
+                },
+              }}
+            anchor="left"
             open={open}
             onOpen={onOpen}
             onClose={onClose}
+            disableBackdropTransition={!iOS}
+            disableDiscovery={iOS}
         >
+            <DrawerHeader>
+                <IconButton onClick={onClose} sx={{color: '#fff'}}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </DrawerHeader>
+            <Divider sx={{borderColor: 'rgba(255,255,255,0.2)'}}/>
             <List>
                 {categories.map(({id, name}) => (
                     <ListItem button key={id}>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -37,7 +37,7 @@ const NavBar = () =>  {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div">
-            E-Commerce
+            eCommerce
           </Typography>
           <ProductSearch />
           <Box sx={{ flexGrow: 1 }} />
