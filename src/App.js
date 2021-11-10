bb78425de2ddd05e8f07431b9b999e5fb7381243
Gonzalo Bernad <gonzalobernad60@gmail.com>
2021-11-09 23:20:28 -0300

    Agregamos el metodo map en un hook useEffect() para simular la carga de un objeto que viene de un endpoint

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const theme = createMuiTheme({
     }
   }
 })
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
--- src/components/catalogo/catalogo.jsx
+++ src/components/catalogo/catalogo.jsx
@@ -1,15 +1,30 @@
-import { Button, Container, Box } from '@mui/material';
-import { useState } from 'react';
+import { Button, Container, Box, Typography, List, ListItem } from '@mui/material';
+import { useEffect, useState } from 'react';
 import TitleComponent from './title/title';
 
 const Catalogo = (props) => {
     const [name, setName] = useState('Gonzalo');
     const [lastName, setLastName] = useState('Bernad');
+    const [edad, setEdad] = useState(33);
+    const [users, setUsers] = useState([]);
+    
+    // useEffect(() => {
+    //     const users = [
+    //         {id: 1, name: 'Alfredo'},
+    //         {id: 2, name: 'Gustavo'},
+    //         {id: 3, name: 'Javier'}
+    //     ]
+    //     setUsers(users);
+    // },[]);
 
     function updateName(){
         setName('Carlos');
         setLastName('Gutierrez')
         console.log(new Date())
+    }
+
+    function setAge(){
+        setEdad(edad + 1);
     }
     
     return ( 
@@ -22,11 +37,23 @@ const Catalogo = (props) => {
                 }}
             >
                 <TitleComponent name={name} lastName={lastName}/>
+                <Typography variant='h4'>{edad} años</Typography>
                 <Button
                     variant="contained"
                     size="large"
                     onClick={updateName}
-                >Cambiar Nombre</Button>
+                >Nombre</Button>
+
+                <Button
+                    sx={{ml: 2}}
+                    variant="contained"
+                    size="large"
+                    onClick={setAge}
+                >Edad</Button>
+
+                <List>
+                    {users.map(({id, name}) => <ListItem key={id}> {name}</ListItem>)}
+                </List>
             </Box>
         </Container>
     );
